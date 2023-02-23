f6181ee0c7e228c8928a911de9552982f672cbeb
Lucas Zawadneak <lucaszawadneak@gmail.com>
2023-02-23 20:04:36 -0300

    fix tfidf correto

--- indexer.cpp
+++ indexer.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <unordered_map>
 #include <vector>
+#include <ctgmath>
 
 #include "hash.cpp"
 #include "binary_tree.cpp"
@@ -86,6 +87,26 @@ int main(int argc, char *argv[])
         cout << "Buscando as " << searchKey << " palavras mais frequentes" << endl;
         find_n_max(hash_table, size, stoi(searchKey));
         return 1;
+    }
+
+    if (mode == "--search")
+    {
+        int documents = 2;
+
+        int occurrences = search(hash_table, searchKey, size);
+        int is_in_documents = occurrences != 0 ? 1 : 0;
+        float termFrequency = occurrences / (float)size;
+        float inverseDocumentFrequency = log10(documents / is_in_documents);
+
+        float tf_idf = termFrequency * inverseDocumentFrequency;
+
+        cout << "Palavra: " << searchKey << endl;
+        cout << "Documentos: " << documents << " documentos" << endl;
+        cout << "Em quantos documentos a palavra aparece: " << is_in_documents << " documentos" << endl;
+        cout << "Ocorrencias: " << occurrences << " ocorrências" << endl;
+        cout << "Frequencia: " << termFrequency << " ocorrências" << endl;
+        cout << "Frequencia inversa: " << inverseDocumentFrequency << " ocorrências" << endl;
+        cout << "TF-IDF: " << tf_idf << endl;
     }
 
     return 0;
